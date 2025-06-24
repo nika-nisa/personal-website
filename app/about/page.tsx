902e6b4c0fcb530dabe70919d7e954faa020f7b4
nika <anika.mabano@gmail.com>
2025-06-23 21:40:41 -0400

    formatting and wording

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,27 +1,28 @@
 export default function AboutPage() {
     return (
         <main className="min-h-screen flex flex-col items-center justify-center p-4 font-mono">
-            <h1 className="hover:text-4xl transition-all duration-300 text-3xl mb-2">about</h1>
-            <p className="max-w-xl text-center text-lg mb-6"> Hi! I'm Anika, a hobbyist UX designer that's into computational biology. </p>
+            <h1 className="hover:text-4xl transition-all duration-300 text-3xl mb-6">about</h1>
+            <p className="max-w-xl text-center text-lg mb-35 text-gray-500"> Hi! I'm Anika, a hobbyist UX designer that's into computational biology. </p>
 
-            <h2 className="text-2xl mb-2">Skills & Expertise</h2>
+          
+            <h2 className="text-2xl mb-6">Skills & Expertise</h2>
 
-            <ul className="list-disc list-inside mb-6">
+            <ul className="list-disc list-inside mb-6 text-gray-500">
                 <li>UX & UI Design (Figma)</li>
                 <li>Frontend Development (React, Next.js, TypeScript)</li>
                 <li>Computational Biology & Data Analysis</li>
                 <li>Python & Bioinformatics Tools</li>
             </ul>
 
-            <h2 className="text-2xl mb-2 mx-auto px-6">My Vision</h2>
-            <p className="mb-2 mx-auto px-25">
+            <h2 className="text-2xl mb-6 mx-auto px-6">My Vision</h2>
+            <p className="mb-2 mx-auto px-25 text-gray-500">
                 I'm driven by my love for elegant design systems and technological solutions that bridge biology and computation!
             </p>
 
-            <p className="mx-auto mb-6 px-50">
+            <p className="mx-auto mb-6 px-50 text-gray-500">
                 Contact me via instagram DM (the way you found this page!) or reach out via <a href="anika.mabano@gmail.com"> email @anika.mabano@gmail.com</a>
             </p>
-
+   
 
 
             
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Navbar() {
     return (
-        <nav className="w-full flex justify-center gap-6 py-4 font-mono text-lg border-b">
+        <nav className="position: fixed w-full flex justify-center gap-9 py-4 font-mono text-lg border-b">
         <Link href="/" className="hover:underline">Home</Link>
         <Link href="/projects" className="hover:underline">Projects</Link>
         <Link href="/about" className="hover:underline">About</Link>
