63422fce6a4f21d072f0a68ea0f966553248cb0d
nika <anika.mabano@gmail.com>
2025-07-12 17:26:49 -0400

    interface updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,67 @@
 export default function HomePage() {
   return (
-    <main className="bg-grid min-h-screen flex flex-col items-center justify-center p-4 font-mono">
-      <h1 className="hover:text-4xl transition-all duration-300 text-3xl mb-2 text-black">
-        anika nina
-      </h1>
+    <main className="bg-grid min-h-screen py-20 px-20 ">
 
-      <div className="flex w-72 gap-4 items-center">
-      <p className="max-w-xl text-center text-lg text-neutral-500">
+      <div className=
+      "flex flex-col items-center font-mono">
+
+        <h1 
+        className="mb-12 text-3xl text-black">
+        hi there! </h1>
+
+        <p
+        className="px-20">
+        My name is anika, and i'm a senior in high school based in the greater Boston area. </p>
+
+        <p
+        className="px-20 py-5 text-neutral-500">
+        still figuring out where home base for the next few years will be... 
+        <span className="text-[#d5aeb0]"> *college!* </span> 
+        but i intend to pursue b.s. in
+        <span className="text-[#c2191f]"> computational biology </span> 
+        and minor in 
+        <span className="text-[#c2191f]"> philosophy</span> 
+        . 
+        </p>
+
+        <p className="py-5">
+          why this odd combo?
+        </p>
+
+        <p
+        className="px-20 py-5 text-neutral-500">
+        it pretty much sums up who i am (at least for now!). i’m obsessed with systems—
+        <span className="text-[#c2191f]">  biological, computational, and human.</span> 
+        </p>
+
+
+      
+
+
+      </div>
+
+    </main>
+  );
+}
+
+{/* 
+
+   <main className="bg-grid min-h-screen flex flex-col items-center justify-center p-4 font-mono">
+
+      
+
+    </main>
+  
+  
+  <h1 className="hover:text-4xl transition-all duration-300 text-3xl mb-2 text-black">
+        hey there!
+      </h1>
+      <div className="flex w-72 gap-4 text-left text-neutral-800">
+        My name is anika, and i'm a senior in high school based in the greater Boston area.
+      </div>
+
+      <div className="flex w-72 gap-4 text-left items-center">
+      <p className="max-w-xl text-lg text-neutral-400">
         I'm a 
         <span className="bg-[#c2191f] mx-1 px-2 py-0.5 rounded-lg opacity-70 text-neutral-200 font-mono text-xs">
           ????
@@ -15,8 +70,4 @@ export default function HomePage() {
         
         working at the intersection of design, biology, and computational thinking
       </p>
-      </div>
-
-    </main>
-  );
-}
+      </div> */}
